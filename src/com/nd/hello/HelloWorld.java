ce72f113be5547994c4db705002771d24a1583ee
Jason <liangzj1630@163.com>
2016-08-30 18:17:06 +0800

    Print "HelloGithub"

---
+++ src/com/nd/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.nd.hello;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("您好，Github之旅！");
+	}
+
+}
