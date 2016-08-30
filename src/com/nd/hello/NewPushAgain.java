6e3c08e1d318a1dd3cf21ae4bc015918cc734d54
Jason <liangzj1630@163.com>
2016-08-30 21:11:20 +0800

    Add new push again

---
+++ src/com/nd/hello/NewPushAgain.java
@@ -0,0 +1,16 @@
+package com.nd.hello;
+
+import java.util.List;
+
+public class NewPushAgain implements Runnable{
+
+	public NewPushAgain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		System.out.println("new Push Again...");
+	}
+
+}
--- src/com/nd/hello/NewPushFile.java
+++ src/com/nd/hello/NewPushFile.java
@@ -3,12 +3,11 @@ package com.nd.hello;
 public class NewPushFile {
 
 	public NewPushFile() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
 		System.out.println("新提交文件...");
-
 	}
 
 }
